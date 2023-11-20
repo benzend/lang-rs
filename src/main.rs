18a8959a96e9b623dddaae2e5682f2c48f3ff852
Benjamin Scott <benjamin.scottt.dev@gmail.com>
2023-11-19 19:54:29 -0700

    refactor: move to own parser

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use regex::Regex;
+use regex::{Error, Regex};
 
 pub const EXAMPLE: &'static str = "
 if true {
@@ -8,7 +8,8 @@ if true {
 }
 ";
 
-const REGEX: &'static str = r"((?:-|\b)\d+\b|[=;{}]|\[\]|\[deprecated\]|\b[A-Za-z_][A-Za-z0-9_]*\b|\/\/.*|\s+)";
+const REGEX: &'static str =
+    r"((?:-|\b)\d+\b|[=;{}]|\[\]|\[deprecated\]|\b[A-Za-z_][A-Za-z0-9_]*\b|\/\/.*|\s+)";
 const IDENTIFIER: &'static str = r"/^[A-Za-z_][A-Za-z0-9_]*$";
 const WHITESPACE: &'static str = r"/^\/\/.*|\s+$";
 const EQUALS: &'static str = r"/^=$/";
@@ -22,59 +23,82 @@ const ENUM_KEYWORD: &'static str = r"/^enum$/";
 const STRUCT_KEYWORD: &'static str = r"/^struct$/";
 
 fn main() {
-    let tokens = tokenize(EXAMPLE);
+    let parser = Parser::new().unwrap();
+    let tokens = parser.tokenize(EXAMPLE);
 
     println!("{:?}", tokens);
 }
 
+pub struct Parser {
+    reg_split: Regex,
+    reg_identifier: Regex,
+    reg_whitespace: Regex,
+    reg_equals: Regex,
+    reg_end_of_file: Regex,
+    reg_semicolon: Regex,
+    reg_integer: Regex,
+    reg_left_brace: Regex,
+    reg_right_brace: Regex,
+    reg_array_token: Regex,
+    reg_enum_keyword: Regex,
+    reg_struct_keyword: Regex,
+}
+
+impl Parser {
+    pub fn new() -> Result<Parser, Error> {
+        Ok(Parser {
+            reg_split: Regex::new(REGEX).unwrap(),
+            reg_identifier: Regex::new(REGEX).unwrap(),
+            reg_whitespace: Regex::new(REGEX).unwrap(),
+            reg_equals: Regex::new(REGEX).unwrap(),
+            reg_end_of_file: Regex::new(REGEX).unwrap(),
+            reg_semicolon: Regex::new(REGEX).unwrap(),
+            reg_integer: Regex::new(REGEX).unwrap(),
+            reg_left_brace: Regex::new(REGEX).unwrap(),
+            reg_right_brace: Regex::new(REGEX).unwrap(),
+            reg_array_token: Regex::new(REGEX).unwrap(),
+            reg_enum_keyword: Regex::new(REGEX).unwrap(),
+            reg_struct_keyword: Regex::new(REGEX).unwrap(),
+        })
+    }
+
+    pub fn tokenize(&self, buffer: &str) -> Result<Vec<Token>, std::io::Error> {
+        let mut tokens: Vec<Token> = Vec::new();
+
+        let mut column = 0;
+        let mut line = 0;
+
+        let mut results: Vec<Token> = Vec::new();
+        for (_, [part]) in self.reg_split.captures_iter(buffer).map(|c| c.extract()) {
+            println!("part: {}", part);
+            if self.reg_identifier.is_match(part) {
+                if self.reg_struct_keyword.is_match(part) {
+                    tokens.push(Token::StructKeyword(Column(1), Line(1)))
+                }
+            }
+        }
+
+        for (i, part) in results.iter().enumerate() {}
+
+        Ok(tokens)
+    }
+}
+
 #[derive(Debug)]
 pub enum Token {
-    Identifier,
-    Equals,
-    EndOfFile,
-    Semicolon,
-    Integer,
-    LeftBrace,
-    RightBrace,
-    ArrayToken,
-    EnumKeyword,
-    StructKeyword
+    Identifier(Column, Line),
+    Equals(Column, Line),
+    EndOfFile(Column, Line),
+    Semicolon(Column, Line),
+    Integer(Column, Line),
+    LeftBrace(Column, Line),
+    RightBrace(Column, Line),
+    ArrayToken(Column, Line),
+    EnumKeyword(Column, Line),
+    StructKeyword(Column, Line),
 }
 
-struct TokenData {
-    token: Token,
-    line: u128,
-    column: u128
-}
-
-pub fn tokenize(text: &str) -> Result<Vec<Token>, std::io::Error> {
-    let reg_parse_text = Regex::new(REGEX).unwrap();
-    let reg_identifier = Regex::new(IDENTIFIER).unwrap();
-    let reg_whitespace = Regex::new(WHITESPACE).unwrap();
-    let req_equals = Regex::new(EQUALS).unwrap();
-    let reg_end_of_file = Regex::new(END_OF_FILE).unwrap();
-    let reg_semicolon = Regex::new(SEMICOLON).unwrap();
-    let reg_integer = Regex::new(INTEGER).unwrap();
-    let reg_left_brace = Regex::new(LEFT_BRACE).unwrap();
-    let reg_right_brace = Regex::new(RIGHT_BRACE).unwrap();
-    let reg_array_token = Regex::new(ARRAY_TOKEN).unwrap();
-    let reg_enum_keyword = Regex::new(ENUM_KEYWORD).unwrap();
-    let reg_struct_keyword = Regex::new(STRUCT_KEYWORD).unwrap();
-
-    let mut tokens: Vec<TokenData> = Vec::new();
-
-    let mut column = 0;
-    let mut line = 0;
-
-    let mut results: Vec<&str> = Vec::new();
-    for (_, [part])  in reg_parse_text.captures_iter(text).map(|c| c.extract()) {
-        println!("part: {}", part);
-        results.push(part);
-    }
-
-    for (i, part) in results.iter().enumerate() {
-
-    }
-
-    Ok(Vec::new())
-}
+#[derive(Debug)]
+pub struct Column(usize);
+#[derive(Debug)]
+pub struct Line(usize);
